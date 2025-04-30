bdd3bb94c8e5ef7d0f816aa1314271b5ff8f5362
rainot <olga24@onet.pl>
2025-04-30 11:16:44 +0200

    Add pictures

--- app/src/main/java/com/example/bartendingmobileapp/CocktailDetailsActivity.kt
+++ app/src/main/java/com/example/bartendingmobileapp/CocktailDetailsActivity.kt
@@ -1,225 +1,35 @@
 package com.example.bartendingmobileapp
 
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.activity.ComponentActivity
-import android.os.Bundle
-import android.telephony.VisualVoicemailSmsFilterSettings
-import android.util.Log
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.material3.Button
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.platform.LocalConfiguration
-import kotlinx.coroutines.delay
 
 
 class CocktailDetailsActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val cocktailName = intent.getStringExtra("cocktail_name")
+        val cocktailType = intent.getStringExtra("cocktail_type")
+
+        val sourceList = when (cocktailType) {
+            "alcoholic" -> alcoholicCocktails
+            "non_alcoholic" -> nonAlcoholicCocktails
+            else -> emptyList()
+        }
+        val cocktail = sourceList.find { it.name == cocktailName }
+
+
         Log.d("CocktailDetails", "Cocktail Name Passed: $cocktailName")
         setContent {
-            MaterialTheme{
-                CocktailDetails(cocktailName)
-            }
-
-        }
-    }
-}
-
-@Composable
-fun CocktailDetails(cocktailName: String?) {
-    val cocktail = cocktails.find { it.name == cocktailName }
-    if (cocktail == null) {
-        Text("Can't find this cocktail", modifier = Modifier.padding(16.dp))
-        return
-    }
-    Box(modifier = Modifier.padding(20.dp)) {
-        val configuration = LocalConfiguration.current
-        if (configuration.screenWidthDp > configuration.screenHeightDp) {
-            // Horizontal view
-            Row(modifier = Modifier.fillMaxSize()) {
-                LazyColumn(modifier = Modifier.weight(1f).padding(20.dp)) {
-                    item {
-                        Text(text = cocktail.name, style = MaterialTheme.typography.headlineMedium)
-                        Spacer(modifier = Modifier.height(10.dp))
-                        Text(
-                            text = "Ingredients:",
-                            style = MaterialTheme.typography.bodyLarge,
-                            fontWeight = FontWeight.Bold
-                        )
-                    }
-                    items(cocktail.ingredients.size) { index ->
-                        Text(text = "- ${cocktail.ingredients[index]}")
-                    }
-                    item {
-                        Spacer(modifier = Modifier.height(10.dp))
-                        Text(
-                            text = "Recipe:",
-                            style = MaterialTheme.typography.bodyLarge,
-                            fontWeight = FontWeight.Bold
-                        )
-                        Text(text = cocktail.recipe)
-                    }
-                }
-                Column(
-                    modifier = Modifier
-                        .weight(1f)
-                        .padding(16.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text(
-                        text = "Timer:",
-                        style = MaterialTheme.typography.bodyLarge,
-                        fontWeight = FontWeight.Bold,
-                        modifier = Modifier.padding(16.dp)
-                    )
-                    Timer(minutes = 0, seconds = 30)
-                }
-            }
-        } else {
-            // Vertical view
-            Column(modifier = Modifier.fillMaxSize()) {
-                LazyColumn(modifier = Modifier.weight(1f).padding(20.dp)) {
-                    item {
-                        Text(text = cocktail.name, style = MaterialTheme.typography.headlineMedium)
-                        Spacer(modifier = Modifier.height(10.dp))
-                        Text(
-                            text = "Ingredients:",
-                            style = MaterialTheme.typography.bodyLarge,
-                            fontWeight = FontWeight.Bold
-                        )
-                    }
-                    items(cocktail.ingredients.size) { index ->
-                        Text(text = "- ${cocktail.ingredients[index]}")
-                    }
-                    item {
-                        Spacer(modifier = Modifier.height(10.dp))
-                        Text(
-                            text = "Recipe:",
-                            style = MaterialTheme.typography.bodyLarge,
-                            fontWeight = FontWeight.Bold
-                        )
-                        Text(text = cocktail.recipe)
-                    }
-                }
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(16.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text(
-                        text = "Timer:",
-                        style = MaterialTheme.typography.bodyLarge,
-                        fontWeight = FontWeight.Bold,
-                        modifier = Modifier.padding(16.dp)
-                    )
-                    Timer(minutes = 0, seconds = 30)
-                }
-            }
-        }
-    }
-}
-@Composable
-fun Timer(minutes: Int, seconds: Int) {
-    var isRunning by remember { mutableStateOf(false) }
-    var isFinished by remember { mutableStateOf(false) }
-    var timeLeftMinutes by remember { mutableStateOf(minutes) }
-    var timeLeftSeconds by remember { mutableStateOf(seconds) }
-    var inputMinutes by remember { mutableStateOf(minutes) }
-    var inputSeconds by remember { mutableStateOf(seconds) }
-
-    Column(modifier = Modifier
-        .fillMaxWidth()
-        .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally)
-    {
-        Row {
-            BasicTextField(
-                value = inputMinutes.toString(),
-                onValueChange = { value ->
-                    inputMinutes = value.filter { it.isDigit() }.toIntOrNull() ?: 0
-                    timeLeftMinutes = inputMinutes
-                },
-                modifier = Modifier.width(50.dp).padding(end = 8.dp),
-                textStyle = MaterialTheme.typography.headlineMedium
-            )
-            Text(":", style = MaterialTheme.typography.headlineMedium)
-            BasicTextField(
-                value = inputSeconds.toString(),
-                onValueChange = { value ->
-                    inputSeconds = value.filter { it.isDigit() }.toIntOrNull()?.coerceAtMost(59) ?: 0
-                    timeLeftSeconds = inputSeconds
-                },
-                modifier = Modifier.width(50.dp).padding(start = 8.dp),
-                textStyle = MaterialTheme.typography.headlineMedium
-            )
-        }
-        Spacer(modifier = Modifier.height(10.dp))
-        Text(
-            text = String.format("%02d:%02d", timeLeftMinutes, timeLeftSeconds),
-            style = MaterialTheme.typography.headlineLarge,
-            modifier = Modifier.padding(10.dp)
-        )
-        Spacer(modifier = Modifier.height(10.dp))
-        Button(onClick = {
-            isRunning = !isRunning
-            isFinished = false
-        }) {
-            Text(if (isRunning) "Pause" else "Start")
-        }
-        Spacer(modifier = Modifier.height(10.dp))
-        Text(
-            if(isFinished){
-                "Time's up!"
-            } else {
-                if (isRunning) {
-                    "Timer is running"
+            MaterialTheme {
+                if (cocktail != null) {
+                    CocktailDetails(cocktail)
                 } else {
-                    "Timer is paused"
+                    Text("Cocktail not found")
                 }
             }
-        )
     }
-
-    LaunchedEffect(isRunning) {
-        while (isRunning && (timeLeftMinutes > 0 || timeLeftSeconds > 0)) {
-            delay(1000)
-            if (timeLeftSeconds > 0) {
-                timeLeftSeconds--
-                if (timeLeftSeconds == 0 && timeLeftMinutes == 0) {
-                    isFinished = true
-                    isRunning = false
-                    timeLeftMinutes = inputMinutes
-                    timeLeftSeconds = inputSeconds
-                }
-            } else {
-                timeLeftMinutes--
-                timeLeftSeconds = 59
-
-            }
-        }
-    }
-}
+}}
--- app/src/main/java/com/example/bartendingmobileapp/MainActivity.kt
+++ app/src/main/java/com/example/bartendingmobileapp/MainActivity.kt
@@ -4,16 +4,21 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.BackHandler
+
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
@@ -24,43 +29,79 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
 
+data class Cocktail(val name: String, @DrawableRes val imageResId: Int, val ingredients: List<String>, val recipe: String)
 
-data class Cocktail(val name: String, val ingredients: List<String>, val recipe: String)
-
-val cocktails = listOf(
-    Cocktail("Mojito", listOf("60 ml white rum", "juice of 1 lime", "5 mint leaves", "1 tsp granulated sugar", "soda water to taste", "ice"),
+val alcoholicCocktails = listOf(
+    Cocktail("Mojito", R.drawable.mohito, listOf("60 ml white rum", "juice of 1 lime", "5 mint leaves", "1 tsp granulated sugar", "soda water to taste", "ice"),
         "STEP 1:\n Muddle the lime juice, sugar and mint leaves in a small jug, crushing the mint as you go – you can use the end of a rolling pin for this. Pour into a tall glass and add a handful of ice.\n" +
                 "STEP 2:\n Pour over the rum, stirring with a long-handled spoon. Top up with soda water, garnish with mint and serve."+
                 "\nText to show that scrolling is possible\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na"),
-    Cocktail("Margarita", listOf("50 ml tequila", "20 ml triple Sec", "ice", "25 ml lime juice", "salt"),
+    Cocktail("Margarita", R.drawable.margarita, listOf("50 ml tequila", "20 ml triple Sec", "ice", "25 ml lime juice", "salt"),
         "STEP 1:\n Sprinkle a few teaspoons of salt over the surface of a small plate or saucer. Rub one wedge of lime along the rim of a tumbler and then dip it into the salt so that the entire rim is covered.\n" +
                 "STEP 2:\n Fill a cocktail shaker with ice, then add the tequila, lime juice and triple sec. Shake until the outside of the shaker feels cold.\n" +
                 "STEP 3:\n Strain the mix into the prepared glass over fresh ice. Serve with a wedge of lime."),
-    Cocktail("Old Fashioned", listOf("60 ml whiskey", "2 tsp sugar", "1-2 dashes bitters", "splash of water", "soda water (optional)", "orange slice", "maraschino cherry (optional"),
+    Cocktail("Old Fashioned", R.drawable.old_fashioned, listOf("60 ml whiskey", "2 tsp sugar", "1-2 dashes bitters", "splash of water", "soda water (optional)", "orange slice", "maraschino cherry (optional"),
         "STEP 1\n" +
                 "Put the sugar, bitters and water in a small tumbler. Mix until the sugar dissolves if using granulated. Fill your glass with ice and stir in the whisky. Add a splash of soda water if you like and mix. Garnish with the orange and cherry."),
-    Cocktail("Example1", listOf("ingredient1", "ingredient2", "ingredient3"), "recipe1"),
-    Cocktail("Example2", listOf("ingredient1", "ingredient2", "ingredient3"), "recipe2"),
-    Cocktail("Example3", listOf("ingredient1", "ingredient2", "ingredient3"), "recipe3"),
-    Cocktail("Example4", listOf("ingredient1", "ingredient2", "ingredient3"), "recipe4"),
-    Cocktail("Example5", listOf("ingredient1", "ingredient2", "ingredient3"), "recipe5"),
-    Cocktail("Example6", listOf("ingredient1", "ingredient2", "ingredient3"), "recipe6"),
-    Cocktail("Example7", listOf("ingredient1", "ingredient2", "ingredient3"), "recipe7"),
-    Cocktail("Example8", listOf("ingredient1", "ingredient2", "ingredient3"), "recipe8"),
-    Cocktail("Example9", listOf("ingredient1", "ingredient2", "ingredient3"), "recipe9"),
-    Cocktail("Example10", listOf("ingredient1", "ingredient2", "ingredient3"), "recipe10"),
-    Cocktail("Example11", listOf("ingredient1", "ingredient2", "ingredient3"), "recipe11"),
-    Cocktail("Example12", listOf("ingredient1", "ingredient2", "ingredient3"), "recipe12"),
-    Cocktail("Example13", listOf("ingredient1", "ingredient2", "ingredient3"), "recipe13"),
-    Cocktail("Example14", listOf("ingredient1", "ingredient2", "ingredient3"), "recipe14"),
-    Cocktail("Example15", listOf("ingredient1", "ingredient2", "ingredient3"), "recipe15")
+    Cocktail("Example1", R.drawable.pink_drink, listOf("ingredient1", "ingredient2", "ingredient3"), "recipe1"),
+    Cocktail("Example2", R.drawable.pink_drink, listOf("ingredient1", "ingredient2", "ingredient3"), "recipe2"),
+    Cocktail("Example3", R.drawable.pink_drink, listOf("ingredient1", "ingredient2", "ingredient3"), "recipe3"),
+    Cocktail("Example4",  R.drawable.pink_drink,listOf("ingredient1", "ingredient2", "ingredient3"), "recipe4"),
+    Cocktail("Example5",  R.drawable.pink_drink,listOf("ingredient1", "ingredient2", "ingredient3"), "recipe5"),
+    Cocktail("Example6", R.drawable.pink_drink, listOf("ingredient1", "ingredient2", "ingredient3"), "recipe6"),
+    Cocktail("Example7", R.drawable.pink_drink, listOf("ingredient1", "ingredient2", "ingredient3"), "recipe7"),
+    Cocktail("Example8", R.drawable.pink_drink, listOf("ingredient1", "ingredient2", "ingredient3"), "recipe8"),
+    Cocktail("Example9", R.drawable.pink_drink, listOf("ingredient1", "ingredient2", "ingredient3"), "recipe9"),
+    Cocktail("Example10", R.drawable.pink_drink, listOf("ingredient1", "ingredient2", "ingredient3"), "recipe10"),
+    Cocktail("Example11",  R.drawable.pink_drink,listOf("ingredient1", "ingredient2", "ingredient3"), "recipe11"),
+    Cocktail("Example12", R.drawable.pink_drink, listOf("ingredient1", "ingredient2", "ingredient3"), "recipe12"),
+    Cocktail("Example13",  R.drawable.pink_drink,listOf("ingredient1", "ingredient2", "ingredient3"), "recipe13"),
+    Cocktail("Example14",  R.drawable.pink_drink,listOf("ingredient1", "ingredient2", "ingredient3"), "recipe14"),
+    Cocktail("Example15",  R.drawable.pink_drink, listOf("ingredient1", "ingredient2", "ingredient3"), "recipe15")
 )
 
 
+val nonAlcoholicCocktails = listOf(
+    Cocktail("Virgin Mojito", R.drawable.mohito, listOf("60 ml non alcoholic white rum", "juice of 1 lime", "5 mint leaves", "1 tsp granulated sugar", "soda water to taste", "ice"),
+        "STEP 1:\n Muddle the lime juice, sugar and mint leaves in a small jug, crushing the mint as you go – you can use the end of a rolling pin for this. Pour into a tall glass and add a handful of ice.\n" +
+                "STEP 2:\n Pour over the rum, stirring with a long-handled spoon. Top up with soda water, garnish with mint and serve."+
+                "\nText to show that scrolling is possible\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na"),
+    Cocktail("Frappe", R.drawable.frappe, listOf("2 double shots cold espresso, or 1/2 cup cold prepared instant espresso or strong brewed coffee", "1/4 cup whole or 2% milk", "2 tablespoons packed light brown sugar", "1/4 teaspoon vanilla extract", "4 ounces ice cubes (about 1 cup)", "Whipped cream, for serving (optional)"),
+        "STEP 1:\n Place 2 double shots cold espresso, or 1/2 cup cold prepared instant espresso or strong brewed coffee, 1/4 cup whole or 2% milk, 2 tablespoons packed light brown sugar, 1/4 teaspoon vanilla extract, and 4 ounces (about 1 cup) ice cubes in a blender. Begin on low speed and increasing to high speed, blend until thick, smooth, and creamy, 20 to 40 seconds.\n" +
+                "STEP 2:\n Pour into a glass and top with whipped cream if desired.\n" +
+                "STEP 3:\n Strain the mix into the prepared glass over fresh ice. Serve with a wedge of lime."),
+    Cocktail("Virgin Old Fashioned", R.drawable.old_fashioned, listOf("60 ml non alcoholic whiskey", "2 tsp sugar", "1-2 dashes bitters", "splash of water", "soda water (optional)", "orange slice", "maraschino cherry (optional"),
+        "STEP 1\n" +
+                "Put the sugar, bitters and water in a small tumbler. Mix until the sugar dissolves if using granulated. Fill your glass with ice and stir in the whisky. Add a splash of soda water if you like and mix. Garnish with the orange and cherry."),
+    Cocktail("Example1", R.drawable.pink_drink, listOf("ingredient1", "ingredient2", "ingredient3"), "recipe1"),
+    Cocktail("Example2", R.drawable.pink_drink, listOf("ingredient1", "ingredient2", "ingredient3"), "recipe2"),
+    Cocktail("Example3", R.drawable.pink_drink, listOf("ingredient1", "ingredient2", "ingredient3"), "recipe3"),
+    Cocktail("Example4",  R.drawable.pink_drink,listOf("ingredient1", "ingredient2", "ingredient3"), "recipe4"),
+    Cocktail("Example5",  R.drawable.pink_drink,listOf("ingredient1", "ingredient2", "ingredient3"), "recipe5"),
+    Cocktail("Example6", R.drawable.pink_drink, listOf("ingredient1", "ingredient2", "ingredient3"), "recipe6"),
+    Cocktail("Example7", R.drawable.pink_drink, listOf("ingredient1", "ingredient2", "ingredient3"), "recipe7"))
+
+
+
+enum class AppTab(val title: String) {
+    Info("Info"),
+    Alcoholic("Alcoholic"),
+    NonAlcoholic("Non-Alcoholic")
+}
 
 
 class MainActivity : ComponentActivity() {
@@ -74,40 +115,125 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
 @Composable
 fun MainScreen() {
     val configuration = LocalConfiguration.current
     val context = LocalContext.current
+    val isTablet = configuration.screenWidthDp > 600
+
+    var selectedTab by remember { mutableStateOf(AppTab.Info) }
+
     var selectedCocktailList by remember { mutableStateOf<List<Cocktail>>(emptyList()) }
 
     BackHandler(enabled = selectedCocktailList.isNotEmpty()) {
         selectedCocktailList= selectedCocktailList.dropLast(1)
     }
 
-    if (configuration.screenWidthDp>600) {
-        Row(modifier = Modifier.fillMaxSize()) {
-            Box(modifier = Modifier.weight(1f)) {
-                CocktailList { cocktail ->
-                    selectedCocktailList = selectedCocktailList + cocktail }
+
+    Scaffold(
+        topBar = {
+            TabRow(selectedTabIndex = selectedTab.ordinal) {
+                AppTab.entries.forEachIndexed { index, tab ->
+                    Tab(
+                        selected = selectedTab.ordinal == index,
+                        onClick = {
+                            selectedTab = tab
+                            selectedCocktailList = emptyList()
+                        },
+                        text = { Text(tab.title) }
+                    )
+                }
             }
-            Box(modifier = Modifier.weight(2f)) {
-                selectedCocktailList.lastOrNull()?.let { CocktailDetails(it.name) }
-            }
+        },
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(WindowInsets.systemBars.asPaddingValues())
+    ) { innerPadding ->
+        Box(modifier = Modifier.padding(innerPadding)) {
+            when (selectedTab) {
+                AppTab.Info -> InfoScreen()
+
+                AppTab.Alcoholic -> {
+                    if (isTablet) {
+                        Row(modifier = Modifier.fillMaxSize()) {
+                            Box(modifier = Modifier.weight(1f)) {
+                                CocktailList(
+                                    cocktails = alcoholicCocktails,
+                                    onCocktailSelected = { cocktail ->
+                                        selectedCocktailList = selectedCocktailList + cocktail
+                                    }
+                                )
+                            }
+                            Box(modifier = Modifier.weight(2f)) {
+                                selectedCocktailList.lastOrNull()?.let {
+                                    CocktailDetails(it)
+                                }
+                            }
+                        }
+                    } else {
+                        CocktailList(
+                            cocktails = alcoholicCocktails,
+                            onCocktailSelected = { cocktail ->
+                                context.startActivity(Intent(context, CocktailDetailsActivity::class.java).apply {
+                                    putExtra("cocktail_name", cocktail.name)
+                                    putExtra("cocktail_type", "alcoholic")
+                                })
+                            }
+                        )
+                    }
+                }
+
+                AppTab.NonAlcoholic -> {
+                    if (isTablet) {
+                        Row(modifier = Modifier.fillMaxSize()) {
+                            Box(modifier = Modifier.weight(1f)) {
+                                CocktailList(
+                                    cocktails = nonAlcoholicCocktails,
+                                    onCocktailSelected = { cocktail ->
+                                        selectedCocktailList = selectedCocktailList + cocktail
+                                    }
+                                )
+                            }
+                            Box(modifier = Modifier.weight(2f)) {
+                                selectedCocktailList.lastOrNull()?.let {
+                                    CocktailDetails(it)
+                                }
+                            }
+                        }
+                    } else {
+                        CocktailList(
+                            cocktails = nonAlcoholicCocktails,
+                            onCocktailSelected = { cocktail ->
+                                context.startActivity(Intent(context, CocktailDetailsActivity::class.java).apply {
+                                    putExtra("cocktail_name", cocktail.name)
+                                    putExtra("cocktail_type", "alcoholic")
+                                })
+                            }
+                        )
+                    }
+                }
+
         }
-    } else {
-        CocktailList { cocktail ->
-            val intent = Intent(context, CocktailDetailsActivity::class.java).apply {
-                putExtra("cocktail_name", cocktail.name)
-            }
-            context.startActivity(intent)
-        }
+    }
+}}
+
+@Composable
+fun InfoScreen() {
+    Column(modifier = Modifier.padding(16.dp)) {
+        Text("Bartending App", style = MaterialTheme.typography.headlineMedium)
+        Spacer(modifier = Modifier.height(8.dp))
+        Text("App made by student with index 155992")
     }
 }
 
 
 @Composable
-fun CocktailList(onCocktailSelected: (Cocktail) -> Unit) {
-    LazyColumn(modifier = Modifier.fillMaxSize()) {
+fun CocktailList(    cocktails: List<Cocktail>,
+                     onCocktailSelected: (Cocktail) -> Unit,
+                     modifier: Modifier = Modifier
+) {
+    LazyVerticalGrid(columns = GridCells.Fixed(2), modifier = Modifier.fillMaxSize()) {
         items(cocktails) { cocktail ->
             Card(
                 modifier = Modifier
@@ -117,6 +243,16 @@ fun CocktailList(onCocktailSelected: (Cocktail) -> Unit) {
                 elevation = CardDefaults.cardElevation(4.dp)
             ) {
                 Column(modifier = Modifier.padding(16.dp)) {
+                    Image(
+                        painter = painterResource(id = cocktail.imageResId),
+                        contentDescription = cocktail.name,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(120.dp)
+                            .clip(MaterialTheme.shapes.medium),
+                        contentScale = ContentScale.Crop
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
                     Text(text = cocktail.name, style = MaterialTheme.typography.headlineSmall)
                 }
             }
